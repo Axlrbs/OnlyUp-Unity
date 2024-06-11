9ab4390aeafc8ffff0e2c7efc1f14c79dfa8e4e3
Axlrbs <146712352+Axlrbs@users.noreply.github.com>
2024-06-11 15:57:14 +0200

    o

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(ConfigurableJoint))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -15,9 +16,17 @@ public class PlayerController : MonoBehaviour
 
     private PlayerMotor motor;
 
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
     private void Start()
     {
         motor = GetComponent<PlayerMotor>();
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
     private void Update()
@@ -47,5 +56,33 @@ public class PlayerController : MonoBehaviour
         float cameraRotationX = xRot * mouseSensitivityY;
 
         motor.RotateCamera(cameraRotationX);
+
+        if(Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
