07665b73af7ab5f43b116c09a5faa89edae09a82
BerkanAkin001 <155959102+BerkanAkin001@users.noreply.github.com>
2024-06-12 14:05:14 +0200

    animations

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(ConfigurableJoint))]
 public class PlayerController : MonoBehaviour
 {
+    private Animator playerAnim;
+
     [SerializeField]
     private float speed = 3f;
-    
+
     [SerializeField]
     private float mouseSensitivityX;
-    
+
     [SerializeField]
     private float mouseSensitivityY;
 
     private PlayerMotor motor;
-
-    private Vector3 playerVelocity;
-
     public Rigidbody rb;
 
     private bool isOnGround = true;
@@ -29,14 +28,17 @@ public class PlayerController : MonoBehaviour
 
 
     private void Start()
-    {
-        motor = GetComponent<PlayerMotor>();
+    { motor = GetComponent<PlayerMotor>();
         rb = GetComponent<Rigidbody>();
+
+        // Configure Rigidbody
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
 
     private void Update()
     {
-        // calculer la vélocité du mvt du joueur
+        // Calculer la vélocité du mouvement du joueur
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
 
@@ -45,44 +47,69 @@ public class PlayerController : MonoBehaviour
 
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
 
-        // application de la vélocité
+        // Application de la vélocité
         motor.Move(velocity);
-        
-        // rotation du joueur
+
+        // Rotation du joueur
         float yRot = Input.GetAxisRaw("Mouse X");
-
         Vector3 rotation = new Vector3(0, yRot, 0) * mouseSensitivityX;
-
         motor.Rotate(rotation);
-        
-        // rotation de la caméra
+
+        // Rotation de la caméra
         float xRot = Input.GetAxisRaw("Mouse Y");
-
         float cameraRotationX = xRot * mouseSensitivityY;
-
         motor.RotateCamera(cameraRotationX);
 
-        if(Cursor.lockState != CursorLockMode.Locked)
+        if (Cursor.lockState != CursorLockMode.Locked)
         {
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        if (Input.GetButtonDown("Jump")&& isOnGround)
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
-            rb.AddForce(new Vector3(0,8,0),ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, 8, 0), ForceMode.Impulse);
             isOnGround = false;
         }
 
-        if(Input.GetButtonDown("Z"))
+        // Gérer les animations avec des triggers
+        if (zMove > 0)
         {
-
+            ResetTriggers();
+            playerAnim.SetTrigger("RunTrigger");
         }
-
+        else if (xMove < 0)
+        {
+            ResetTriggers();
+            playerAnim.SetTrigger("LeftTrigger");
+        }
+        else if (xMove > 0)
+        {
+            ResetTriggers();
+            playerAnim.SetTrigger("RightTrigger");
+        }
+        else if (zMove < 0)
+        {
+            ResetTriggers();
+            playerAnim.SetTrigger("BackTrigger");
+        }
+        else
+        {
+            ResetTriggers();
+        }
+    }
+        private void ResetTriggers()
+    {
+        playerAnim.ResetTrigger("RunTrigger");
+        playerAnim.ResetTrigger("LeftTrigger");
+        playerAnim.ResetTrigger("RightTrigger");
+        playerAnim.ResetTrigger("BackTrigger");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             isOnGround = true;
+        }
     }
 }
